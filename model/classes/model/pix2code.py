56a3880c0a00e281c0aeafd57a093b3fd00d4c14
Rahul Keshervani <30744073+RAHUL-KESHERVANI@users.noreply.github.com>
2019-11-04 11:41:56 +0530

    corrected first line

--- model/classes/model/pix2code.py
+++ model/classes/model/pix2code.py
@@ -1,4 +1,4 @@
-rom __future__ import absolute_import
+from __future__ import absolute_import
 __author__ = 'Tony Beltramelli - www.tonybeltramelli.com'
 
 from keras.layers import Input, Dense, Dropout, \
